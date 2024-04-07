b1dd43fe9e48b385bb534328dd92c19f83420011
Nathan <71136851+arandrollcode@users.noreply.github.com>
2024-04-07 11:42:59 -0500

    merge stuff

--- assets/scripting/buttonNext.js
+++ assets/scripting/buttonNext.js
@@ -1,4 +1,19 @@
-function startTimer() {
+function startTimer0to1() {
     // Redirect to the next page
     window.open('adhd1.html', '_blank');
+}
+
+function startTimer1to2() {
+    // Redirect to the next page
+    window.open('adhd2.html', '_blank');
+}
+
+function startTimer2to3() {
+    // Redirect to the next page
+    window.open('adhd3.html', '_blank');
+}
+
+function startTimer3to4() {
+    // Redirect to the next page
+    window.open('adhd4.html', '_blank');
 }
